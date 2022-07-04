f0c8dd43615ce166b92f51e9afc32165a96801cd
JCaeta <63880187+JCaeta@users.noreply.github.com>
2022-07-03 22:20:53 -0300

    template-express-api updated

--- server.ts
+++ server.ts
@@ -3,7 +3,7 @@ const cors = require('cors');
 var bodyParser = require('body-parser');
 
 const app = express();
-const PORT = process.env.PORT || 3001;
+const PORT = process.env.PORT || 3000;
 app.use(cors({
     origin: "http://localhost:PORT"
 }));
@@ -14,13 +14,12 @@ app.use(bodyParser.urlencoded({
 }));
 
 app.get('/', (req, res) => {
-
+    res.send(true);
 });
 
 app.post('/post-request', (req, res) => {
-
+    let body = req.body;
+    console.log(body);
 });
 
 app.listen(PORT, () => {console.log('Serve started on port ' + PORT)});
-
-
